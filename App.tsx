f452a826856e43b7a403ee581398a1bddfeefcde
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-23 02:04:23 +0800

    resolve conflicts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {
