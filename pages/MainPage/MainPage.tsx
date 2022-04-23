f054f1ac4ce1cac7c2cdba2ebb27d014d466dca9
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-23 18:02:15 +0800

    height fix

--- pages/MainPage/MainPage.tsx
+++ pages/MainPage/MainPage.tsx
@@ -1,26 +1,16 @@
 import "react-native-gesture-handler";
 import React from "react";
 import {
-  SafeAreaView,
   StyleSheet,
-  Text,
-  Dimensions,
-  View,
-  Image,
-  TextInput,
   ScrollView,
 } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import LeftSideBar from "../../components/SideBar/LeftSideBar";
 import RightSideBar from "../../components/SideBar/RightSideBar";
-import { TouchableHighlight } from "react-native-gesture-handler";
 import Message from "../../components/Message";
 import { useInput } from "../../hooks/useInput";
 import AppBar from "../../components/Appbar";
 import BottomBar from "../../components/BottomBar";
-
-const width = Dimensions.get("window").width; //full width
-const height = Dimensions.get("window").height; //full height
 
 interface IMainProps {
   handleAuthPageOpen: () => void;
@@ -45,66 +35,17 @@ const MainPage: React.FC<IMainProps> = ({ navigation }: any) => {
       overshootLeft={false}
       overshootRight={false}
       ref={sideBarRef}
+      containerStyle={{ flexDirection: 'column', flex: 1, backgroundColor: '#f00' }}
+      childrenContainerStyle={{ flex: 1 }}
     >
-      <SafeAreaView style={styles.container}>
-        <AppBar sideBarRef={sideBarRef} />
-        <ScrollView style={{ flex: 1, backgroundColor: "#303030" }}>
-          <Message />
-          <Message />
-        </ScrollView>
-        <BottomBar message={message} />
-      </SafeAreaView>
+      <AppBar sideBarRef={sideBarRef} />
+      <ScrollView contentContainerStyle={{ flex: 1, flexGrow: 12 }} style={{ flex: 1, backgroundColor: "#303030" }}>
+        <Message />
+        <Message />
+      </ScrollView>
+      <BottomBar message={message} />
+
     </Swipeable>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#181A1B",
-    width: width,
-    height: height - 100,
-  },
-  appBar: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#262626",
-    padding: 10,
-  },
-  appBar__menu: {
-    marginRight: 10,
-  },
-
-  chatName: {
-    flex: 1,
-    color: "#fff",
-    fontWeight: "500",
-  },
-
-  bottomBar: {
-    width: "100%",
-    flexDirection: "row",
-    backgroundColor: "#303030",
-    alignItems: "center",
-    padding: 10,
-  },
-  buttonImageIconStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-  },
-  input: {
-    flex: 1,
-    fontSize: 30,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    color: "#fff",
-    backgroundColor: "#161819",
-    borderRadius: 20,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 export default MainPage;
