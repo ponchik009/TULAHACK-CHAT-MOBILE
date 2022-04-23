25539fb78628eeff414fde2dc21fbb7f2447e5b8
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-23 16:45:38 +0800

    stack navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { SafeAreaView, StyleSheet, View, Text, Dimensions } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, Dimensions, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import AuthPage from "./pages/AuthPage/AuthPage";
@@ -10,15 +10,13 @@ import { NativeBaseProvider } from "native-base";
 const width = Dimensions.get("window").width; //full width
 const height = Dimensions.get("window").height; //full height
 
-// const swipeFromLeftOpen = () => {
-//   alert("Swipe from left");
-// };
-// const swipeFromRightOpen = () => {
-//   alert("Swipe from right");
-// };
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [isAuth, setIsAuth] = React.useState(false);
+  const [isAuth, setIsAuth] = React.useState(true);
 
   // зарефакторить в отдельный хук?
   const handleAuthPageOpen = () => {
@@ -29,16 +27,17 @@ const App = () => {
   };
 
   return (
-    <NativeBaseProvider>
-      <StatusBar style="auto" />
-      <SafeAreaView style={styles.wrapper}>
-        {isAuth ? (
-          <MainPage handleAuthPageOpen={handleAuthPageOpen} />
-        ) : (
-          <AuthPage handleMainPageOpen={handleMainPageOpen} />
-        )}
-      </SafeAreaView>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={MainPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Profile" component={AuthPage}
+          options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 const styles = StyleSheet.create({
